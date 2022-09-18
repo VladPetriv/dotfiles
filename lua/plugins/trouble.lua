8e71960d221709ea768839fc2c4ea85ea50f9566
Vlad Petriv <80276277+VladPetriv@users.noreply.github.com>
2022-09-18 16:36:00 +0300

    Create trouble.lua

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,9 @@
+require('trouble').setup {
+    position = "right", -- position of the list can be: bottom, top, left, right
+    width = 54, -- width of the list when position is left or right
+    icons = true, -- use devicons for filenames
+    fold_open = "", -- icon used for open folds
+    fold_closed = "", -- icon used for closed folds
+    group = true, -- group results by file
+    padding = true, -- add an extra new line on top of the list
+}
