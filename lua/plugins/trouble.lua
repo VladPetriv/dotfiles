bed32d5eb23d8500d414a2b19847b33c9e4233ec
Vlad Petriv <80276277+VladPetriv@users.noreply.github.com>
2022-09-18 16:55:38 +0300

    Update trouble.lua

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,6 +1,6 @@
 require('trouble').setup {
     position = "right", -- position of the list can be: bottom, top, left, right
-    width = 54, -- width of the list when position is left or right
+    width = 100, -- width of the list when position is left or right
     icons = true, -- use devicons for filenames
     fold_open = "", -- icon used for open folds
     fold_closed = "", -- icon used for closed folds
