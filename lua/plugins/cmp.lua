b4781fe716fd0d67f462ec460345d578b45b1e37
Vlad Petriv <80276277+VladPetriv@users.noreply.github.com>
2022-09-30 17:07:05 +0300

    chore: remove commented code

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,26 +18,11 @@ cmp.setup({
     ['<Tab>'] = cmp.mapping.confirm({ select = true }),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-u>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
   }),
-  formatting = {
-    -- format = function(entry, vim_item)
-    --   vim_item.kind = lspkind.presets.default[vim_item.kind]
-    --   local menu = source_mapping[entry.source.name]
-    --   if entry.source.name == 'cmp_tabnine' then
-    --     if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-    --       menu = entry.completion_item.data.detail .. ' ' .. menu
-    --     end
-    --   vim_item.kind = ''
-    --   end
-    --   vim_item.menu = menu
-    --   return vim_item
-    -- end
-  },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
   }, {
     { name = 'buffer' },
-  })
+  }),
 })
