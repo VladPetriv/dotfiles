b93cd9a5044b079b6e02749e3c9acba8bcdc4075
Vlad Petriv <vladpetriv05@gmail.com>
2022-11-25 21:15:32 +0200

    chore: remove theme configuration in options

--- lua/options.lua
+++ lua/options.lua
@@ -27,20 +27,3 @@ o.updatetime = 50
 o.showmode = false
 o.termguicolors = true
 vim.g.mapleader = ";"
-
-require("gruvbox").setup({
-  bold = true,
-  italic = false,
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = false,
-})
-vim.cmd("colorscheme gruvbox")
