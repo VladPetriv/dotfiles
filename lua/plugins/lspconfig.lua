ba02b2f806226825f95f3b2a682250f353affeb6
Vlad Petriv <vladpetriv05@gmail.com>
2022-12-17 19:40:14 +0200

    feat: add configuration for golangci-lint lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,5 +1,6 @@
 local lspconfig = require('lspconfig')
 local util = require('lspconfig/util')
+local configs = require 'lspconfig/configs'
 
 local function config(_config)
   return vim.tbl_deep_extend('force', {
@@ -29,3 +30,18 @@ for type, icon in pairs(signs) do
     vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
 end
 
+
+if not configs.golangcilsp then
+ 	configs.golangcilsp = {
+		default_config = {
+			cmd = {'golangci-lint-langserver'},
+			root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+			init_options = {
+					command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json", "--issues-exit-code=1" };
+			}
+		};
+	}
+end
+lspconfig.golangci_lint_ls.setup {
+	filetypes = {'go','gomod'}
+}
