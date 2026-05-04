9cb93f8e96aedc5b35c57078aea092ae093305d2
Vlad Petriv <vladpetriv05@gmail.com>
2026-05-04 06:50:41 +0200

    fix(nvim): adjust keybind for lsp hover

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -18,7 +18,7 @@ return {
                     map('n', 'gD', vim.lsp.buf.declaration, opts)
                     map('n', 'gi', vim.lsp.buf.implementation, opts)
                     map('n', 'gr', vim.lsp.buf.references, opts)
-                    map('n', 'K', vim.lsp.buf.hover, opts)
+                    map('n', 'gt', vim.lsp.buf.type_definition, opts)
                     map('n', '<leader>rn', vim.lsp.buf.rename, opts)
                     map('n', '<leader>ca', vim.lsp.buf.code_action, opts)
 
